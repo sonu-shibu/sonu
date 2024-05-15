84bd9a84b66977f436b632198f6812ad7cbdc9b5
sonu-shibu <165980783+sonu-shibu@users.noreply.github.com>
2024-05-15 11:26:14 +0530

    it is a python pattern printing program

---
+++ pattern.py
@@ -0,0 +1,33 @@
+
+
+
+"""exp19
+a=int(input("enter number="))
+for i in range(1,a+1):
+    for j in range(1,i+1):
+        print(j,end=' ')
+    print()
+"""
+"""exp18
+a=int(input("enter number="))
+for i in range(1,a+1):
+    for j in range(i):
+        print(i,end=' ')
+    print()
+"""
+"""exp20
+
+a=int(input("enter number="))
+for i in range(a,0,-1):
+    for j in range(i):
+        print('*',end=' ')
+    print()
+"""
+"""exp17
+a=int(input("enter number="))
+for i in range(1,a+1):
+    for j in range(i):
+        print('*',end=' ')
+    print()
+"""
+
